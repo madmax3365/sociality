c824aa47cb88a4db9bf5e5285ad66380b66bc159
Gagik.Novembit <gagik.novembit@gmail.com>
2018-06-06 19:16:59 +0400

    Added React - Webpack boilerplate

---
+++ client/src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div>
+				<h2>
+				React App Boilerplate
+				</h2>
+			</div>
+		);
+	}
+}
