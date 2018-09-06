410ba8cc6929a08c7405b8a68d25ec0a8e0fdbaf
Gagik.Novembit <gagik.novembit@gmail.com>
2018-09-06 12:10:00 +0400

    Prepare to heroku

--- server.js
+++ server.js
@@ -22,10 +22,7 @@ app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 
 // MongoDB connection
-mongoose
-	.connect(db)
-	.then(() => console.log('Connected!'))
-	.catch(e => console.log(e));
+mongoose.connect(db);
 
 // Passport middleware
 app.use(passport.initialize());
@@ -39,10 +36,10 @@ app.use('/api/posts', posts);
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
-	app.use(express.static('client/build'));
+	app.use(express.static('client/dist'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 	});
 }
 
